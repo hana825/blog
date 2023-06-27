5de823adcd32b1222d4e0dc4e38414c49ed54695
hana825 <hana825@naver.com>
2023-06-27 21:45:42 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 const App = () => {
   return (
     <div className='container'>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/blog/:id' element={<Blog/>} />
